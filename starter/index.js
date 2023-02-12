fd625e1f455730b661b14716f0f93feaac4bcac6
George Bizzell <george.bizzell@gmail.com>
2023-02-12 17:49:21 +0000

    First commit with test question

--- starter/index.js
+++ starter/index.js
@@ -3,8 +3,30 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+// Not sure if this is required given the line above??
+import inquirer from 'inquirer';
+
+// inquirer package install - https://www.npmjs.com/package/inquirer
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name:"title",
+        message: "What is the name of the application?"
+    }
 
 ];
 
